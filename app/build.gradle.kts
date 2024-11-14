d5966dacaf94e43830a71c35bf1e4a6e07262574
Erikson <eriksonribeiro84@gmail.com>
2024-11-14 00:21:15 -0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.roomdemo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.roomdemo"
+        minSdk = 24
+        //noinspection OldTargetApi
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    // Room and Lifecycle dependencies
+    implementation ("androidx.room:room-runtime:2.3.0")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    //kotlin extensions for coroutine support with room
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //kotlin extension for coroutine support with activities
+    implementation ("androidx.activity:activity-ktx:1.3.1")
+
+
+    implementation ("androidx.core:core-ktx:1.6.0")
+    implementation ("androidx.appcompat:appcompat:1.3.1")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
+    testImplementation ("junit:junit:4.+")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+}
